0b0f9fd0be3eae39c1bef16bb531e9790c653930
axe_mello <akselcruses@gmail.com>
2024-03-25 13:28:58 +0530

    final

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 (function() {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    for (var i = 0;  i <names.length; i++) {
-    var f = names[i].charAt(0).toLowerCase;
-    if ( f === "j") {
-        byeSpeaker.speak(names[i]);
-    } else {
-        helloSpeaker.speak(names[i]);
-  }
-}
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
 })();
